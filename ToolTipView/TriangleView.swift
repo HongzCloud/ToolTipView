bf6ce867f7204fdb526efec31230df95ac7e01d6
HongzCloud <jihong21511@gmail.com>
2024-04-22 23:42:31 +0900

    :art: Apple isOpaque 공식문서에 따라 뷰가 완전히 또는 부분적으로 투명한 경우 false로 지정

--- ToolTipView/TriangleView.swift
+++ ToolTipView/TriangleView.swift
@@ -11,6 +11,15 @@ class TriangleView: UIView {
     
     var color: UIColor = .white
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.isOpaque = false
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func draw(_ rect: CGRect) {
         let path = UIBezierPath()
         path.move(to: CGPoint(x: 0, y: 0))
